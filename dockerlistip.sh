ab7bf8753d4409fa05e42c1fc82bc19f12e9463b
azhestov <azhestov@gmail.com>
2024-04-30 20:38:28 +0400

    Update dockerlistip.sh

--- dockerlistip.sh
+++ dockerlistip.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
-# List name, id and IP of docker contaners. Show same for single container if id or name passed as parameter to script
-# Function to output formatted result
+
+# extract container details
+extract_container_details() {
+    container_info=$(docker inspect "$1" 2>/dev/null)
+
+    # container exists?
+    if [ $? -ne 0 ]; then
+        echo "Container '$1' not found"
+        exit 1
+    fi
+
+    # name, short ID, and IP
+    container_name=$(echo "$container_info" | jq -r '.[0].Name' | cut -d'/' -f2)
+    container_id=$(echo "$container_info" | jq -r '.[0].Id')
+    short_container_id=${container_id:0:12} # Get the short container ID
+    container_ip=$(echo "$container_info" | jq -r '.[0].NetworkSettings.Networks | to_entries[0].value.IPAddress // empty')
+
+    if [ -z "$container_ip" ]; then
+        echo "Container '$container_name' has no IP address assigned"
+        exit 1
+    fi
+}
+
+# formatted result
 format_output() {
     container_name="$1"
     short_container_id="$2"
@@ -8,44 +30,24 @@ format_output() {
     awk -v name="$container_name" -v id="$short_container_id" -v ip="$container_ip" 'BEGIN {printf "%-20s\t%-20s\t%-20s\n", name, id, ip}'
 }
 
-# Get a list of containers
+# list of containers
 all_containers=$(docker ps -a --format "{{.Names}}")
 
-# If an argument (container name or ID) is provided
+# argument is provided?
 if [ -n "$1" ]; then
-    # Get one container
-    container_info=$(docker inspect "$1" 2>/dev/null)
+    # Extract container details
+    extract_container_details "$1"
 
-    # Check if the container exists
-    if [ $? -ne 0 ]; then
-        echo "Container '$1' not found"
-        exit 1
-    fi
-
-    container_name=$(echo "$container_info" | jq -r '.[0].Name' | cut -d'/' -f2)
-    container_id=$(echo "$container_info" | jq -r '.[0].Id')
-    short_container_id=${container_id:0:12} # Get the short container ID
-    container_ip=$(echo "$container_info" | jq -r '.[0].NetworkSettings.Networks | to_entries[0].value.IPAddress // empty')
-
-    # IP address not found
-    if [ -z "$container_ip" ]; then
-        echo "Container '$container_name' has no IP address assigned"
-        exit 1
-    fi
-
-    # main
+    # single container
     format_output "$container_name" "$short_container_id" "$container_ip"
 else
-    # If no argument is provided, list all containers
+    # full list
     if [ -z "$all_containers" ]; then
         echo "No containers found"
     else
         printf "%-20s\t%-20s\t%-20s\n" "NAME" "ID" "IP"
         while read -r container_name; do
-            container_info=$(docker inspect "$container_name" 2>/dev/null)
-            container_id=$(echo "$container_info" | jq -r '.[0].Id')
-            short_container_id=${container_id:0:12}
-            container_ip=$(echo "$container_info" | jq -r '.[0].NetworkSettings.Networks | to_entries[0].value.IPAddress // empty')
+            extract_container_details "$container_name"
             format_output "$container_name" "$short_container_id" "$container_ip"
         done <<< "$all_containers"
     fi
